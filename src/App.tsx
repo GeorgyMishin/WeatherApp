81d78897d39126e5ecca3c59a09a45c36cba5cb9
Georgy Mishin <>
2021-03-28 19:24:24 +0300

    Fix android

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,11 @@ import store from './modules/rootStore';
 
 const App: React.FC = () => (
   <View style={styles.container}>
-    <StatusBar barStyle="light-content" />
+    <StatusBar
+      barStyle="light-content"
+      backgroundColor="transparent"
+      translucent
+    />
     <Provider store={store}>
       <Navigation />
     </Provider>
